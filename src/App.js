76b2f9e23f44f6d87db08da6c583313e6bb74991
Juan III De los santos <j3delossantos@outlook.com>
2021-05-01 16:48:19 -0400

    Completed look and feel of the header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header'
+import Home from './components/Home'
 
 
 import './App.css';
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       
         <Header/>
+        <Home/>
     </div>
   );
 }
