763b6267f88cbf8dba041d3a0c4750e0a7bd9c07
Juan III De los santos <j3delossantos@outlook.com>
2021-05-01 18:26:06 -0400

    Completed carousell component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
+import ImgSider from './ImgSider'
 
 function Home() {
     return (
         <Container>
-            Home
+            <ImgSider/>
         </Container>
     )
 }
@@ -14,10 +15,18 @@ export default Home
 const Container = styled.main`
     min-height: calc(100vh - 70px);
     padding: 0 calc(3.5vw + 5px);
+    position: relarive;
 
     &:before{
-        background: url("/images/home/home-background.png") center center cover no-repeat fixed;
-        content: "dd";
+        background: url("/images/home-background.png") center center / cover 
+        no-repeat fixed;
+        content: "";
+        position: absolute;
+        top 0;
+        left 0;
+        right 0;
+        bottom:0;
+        z-index: -1;
     }
 
     
