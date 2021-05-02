c2659f479c3dda237bac91951df8c06e7a5ed848
Juan III De los santos <j3delossantos@outlook.com>
2021-05-02 10:54:22 -0400

    Completed Detail page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -19,16 +19,20 @@ function Detail() {
                 <TrailerButton>
                 <img src="/images/play-icon-white.png"/>
                 <span>TRAILER</span>
-
                 </TrailerButton>
                 <AddButton>
-
+                    <span>+</span>
                 </AddButton>
                 <GroupWatchButton>
-
+                    <img src="/images/group-icon.png" />
                 </GroupWatchButton>
-
             </Controlls>
+            <SubTitle>
+                2018 - 7m - Family, Fantasy, Kids, Animation
+            </SubTitle>
+            <Description>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </Description>
         </Container>
     )
 }
@@ -47,13 +51,13 @@ const Background=styled.div`
     top 0;
     left 0;
     right 0;
-    bottom:0;
+    bottom: 0;
     z-index: -1;
     opacity: 0.8;
 
     img{
-        width:100%;
-        height:100%;
+        width: 100%;
+        height: 100%;
         object-fit: cover;
     }
 
@@ -62,23 +66,24 @@ const Background=styled.div`
 
 const ImageTitle=styled.div`
 
-    height: 30vh;   
+    height: 50vh;   
     min-height: 170px;
     width: 35vw;    
     min-width: 200px;
 
-    img{
-        width:100%;
-        height:100%;
+    img {
+        width: 100%;
+        height: 100%;
         object-fit: contain;
     }
 `
 
 const Controlls=styled.div`
     display: flex;
+    align-items: center;
 `
 
-const PlayButton=styled.div`
+const PlayButton=styled.button`
      
    height: 56px;
    border-radius: 4px;
@@ -90,9 +95,9 @@ const PlayButton=styled.div`
    align-items: center;
    background: rgb(249, 249, 249);
    letter-spacing: 1.8px;
-   cursor-pointer;
+   cursor: pointer;
 
-   &:hover{
+   &:hover {
         background: rgb(198, 198, 198);
    }
 `
@@ -101,9 +106,39 @@ const TrailerButton=styled(PlayButton)`
     border: 1px solid rgb(249, 249, 249);
     color: rgb(249, 249, 249);
 `
-const AddButton=styled.div`
+const AddButton=styled.button`
+   margin-right: 16px;
+   width: 44px;
+   height: 44px;
+   display: flex;
+   align-items: center;
+   justify-content: center;   
+   border: 2px solid white;
+   border-radius: 50%;
+   background-color: rgba(0, 0, 0, 0.6);
+   cursor: pointer;
+   span {
+       font-size: 30px;
+       color: white;
+   }
 
 `
-const GroupWatchButton=styled.div`
+const GroupWatchButton=styled(AddButton)`
+    background-color: rgb(0, 0, 0);
+`
+
+const SubTitle=styled.div`
+   color:rgb(249, 249, 249);
+   font-size: 15px;
+   min-height: 20px;
+   margin-top: 26px;
+
+`
+const Description=styled.div`
+   line-height: 1.4;
+   font-size: 20px;
+   margin-top: 16px;
+   color:rgb(249, 249, 249);
+   width:50vw;
 
 `
